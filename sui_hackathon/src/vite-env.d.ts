6f612dd5b5e60cf1da0756b40b82942bbb0e4535
nictogin <2024havetobeok@gmail.com>
2025-11-30 05:59:21 +0800

    Add TypeScript environment variable types

--- sui_hackathon/src/vite-env.d.ts
+++ sui_hackathon/src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PUSHER_KEY?: string;
+  readonly VITE_PUSHER_CLUSTER?: string;
+  readonly VITE_SPONSOR_API_URL?: string;
+  readonly VITE_GOOGLE_CLIENT_ID?: string;
+  readonly VITE_PROVING_SERVICE_URL?: string;
+  readonly VITE_SALT_SERVICE_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
